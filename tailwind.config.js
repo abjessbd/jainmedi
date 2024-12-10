c91e0c77f85f5b316977dfaf647a9b1c35ea5a11
abjessbd <abjessbd@gmail.com>
2024-12-10 15:44:58 +0600

    Tailwind screen size added for responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,20 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     screens: {
-      sm: "576px",
-      // => @media (min-width: 576px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
       md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      lg: "992px",
-      // => @media (min-width: 992px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
 
-      xl: "1200px",
-      // => @media (min-width: 1200px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
 
-      xxl: "1400px",
-      // => @media (min-width: 1400px) { ... }
+      xxl: "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       fontFamily: {
