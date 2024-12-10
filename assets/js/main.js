afdf5b02a85699d87abd428d3cb40f68b92cc5e1
abjessbd <abjessbd@gmail.com>
2024-12-10 21:35:13 +0600

    Swiper slider added

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,3 +124,27 @@ cross.addEventListener('click', () => {
 })
 
 // =========== Project Slider - 1 =============
+
+// Swiper slider for Service section
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
